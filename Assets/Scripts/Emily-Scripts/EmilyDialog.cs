d478bd63c266558e0d84138e21b0858ce245c964
Emily Bao <emily.yf.bao@gmail.com>
2020-08-29 15:42:22 -0600

    Added better graphics and hat!! :)

--- Assets/Scripts/Emily-Scripts/EmilyDialog.cs
+++ Assets/Scripts/Emily-Scripts/EmilyDialog.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Dialog : MonoBehaviour
+public class EmilyDialog : MonoBehaviour
 {
     public TextMeshProUGUI textDisplay;
     public string[] sentences;
