edde8aba70037ae792feb98466bcd3ba1c4f65e1
AndrewMinyoungKim <akim48415@gmail.com>
2020-08-29 12:45:28 -0400

    Added basic changing scene functionality

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public bool inRange = false;
+    public string levelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inRange) {
+            if(Input.GetMouseButtonDown(0)) {
+                SceneManager.LoadScene(levelName);
+            }
+        }
+    }
+
+    void OnMouseOver() {
+        inRange = true;
+    }
+
+    void OnMouseExit() {
+        inRange = false;
+    }
+}
