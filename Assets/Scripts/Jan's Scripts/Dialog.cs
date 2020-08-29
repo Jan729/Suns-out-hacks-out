5a9dde4f2a5af1b33513c09110a6f627d789e29d
Jan729 <46268176+Jan729@users.noreply.github.com>
2020-08-29 16:46:36 -0400

    Add help text and title screen

---
+++ Assets/Scripts/Jan's Scripts/Dialog.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    public float typingSpeed = 0.02f;
+    public GameObject continueButton;
+    private int index;
+
+    public void StartDialog()
+    {
+        StartCoroutine(Type());
+    }
+
+    public void Start()
+    {
+        continueButton.SetActive(false);
+    }
+    
+    void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    } 
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+    
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } else
+        {
+            continueButton.SetActive(false);
+            textDisplay.text = "";
+        }
+    } 
+
+}
