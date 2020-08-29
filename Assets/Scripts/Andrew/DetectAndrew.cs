f18d4e4d36e54f897b361d75c8e4dfb0320cd150
AndrewMinyoungKim <akim48415@gmail.com>
2020-08-29 18:58:05 -0400

    Added lobby and stuff

---
+++ Assets/Scripts/Andrew/DetectAndrew.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectAndrew : MonoBehaviour
+{
+    public bool inRange;
+    
+    void OnMouseOver() {
+        inRange = true;
+    }
+
+    void OnMouseExit() {
+        inRange = false;
+    }
+}
