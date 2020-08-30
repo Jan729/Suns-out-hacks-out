7f1667f168d9271f297a01cd2e2db3ad418a1418
AndrewMinyoungKim <akim48415@gmail.com>
2020-08-30 00:57:10 -0400

    Added suspect investigations

---
+++ Assets/Scripts/Andrew/SimpleDialogue.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SimpleDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+    //public GameObject StartTrigger;
+    //public bool start = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        continueButton.SetActive(false);
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(StartTrigger.GetComponent<DetectAndrew>().inRange) {
+            if(Input.GetMouseButtonDown(0)) {
+                if(start) {
+                    start = false;
+                    StartCoroutine(Type());
+                }
+            }
+        }*/
+
+        if(textDisplay.text == sentences[index]) {
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type() {
+        foreach(char letter in sentences[index].ToCharArray()) {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence() {
+        continueButton.SetActive(false);
+        if(index < sentences.Length-1) {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }else {
+            textDisplay.text = "";
+            continueButton.SetActive(true);
+            index = 0;
+            StartCoroutine(Type());
+            //start = true;
+        }
+    }
+}
